743bfb6dcf574f37142ecbb8c16a2e72f877f7db
João Saffran <you@example.com>
2016-07-04 09:39:02 -0300

    Begin

---
+++ test/main.cpp
@@ -0,0 +1,34 @@
+#include "../src/AdjacencyMatrix.hpp"
+
+using namespace std;
+using namespace Graph;
+
+int main(int argc, char** argv)
+{
+    AdjacencyMatrix* am = new AdjacencyMatrix(6);
+
+    am->addEdge(0,1,1);
+    am->addEdge(1,0,1);
+
+    am->addEdge(1,2,1);
+    am->addEdge(2,1,1);
+    
+    am->addEdge(2,3,5);
+    am->addEdge(3,2,5);
+    
+    am->addEdge(3,4,1);
+    am->addEdge(4,3,1);
+    
+    am->addEdge(4,5,1);    
+    am->addEdge(5,4,1);    
+    
+    am->addEdge(5,0,1);
+    am->addEdge(0,5,1);    
+    
+
+
+    am->print();
+    printf("%d \n", am->isBipartite());
+
+    return 0;
+}
